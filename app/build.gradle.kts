dcf91b446cc45d669083d76cf8d7bea98520f64c
Huang Zhaohong <huaug9672@gmail.com>
2026-08-28 02:12:33 +0800

    更新版本名稱至 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     minSdk = 26
     targetSdk = 37
     versionCode = 1
-    versionName = "1.0.0"
+    versionName = "0.9.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
